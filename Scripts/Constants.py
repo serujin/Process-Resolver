4a65eed42f677e737b24252f02a0618f14dbacff
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2020-11-04 12:45:56 +0100

    Everything works fine with no priority and IO, need to test that

--- Scripts/Constants.py
+++ Scripts/Constants.py
@@ -14,3 +14,19 @@ FIFO = 0
 SJF = 1
 SRTF = 2
 RR = 3
+#Letter colors
+LETTER_COLORS = {
+    0 : '#FF0000',
+    1 : '#FF7B00',
+    2 : '#FFCC00',
+    3 : '#88FF00',
+    4 : '#00FF44',
+    5 : '#00FFD0',
+    6 : '#00D9FF',
+    7 : '#005EFF',
+    8 : '#0D00FF',
+    9 : '#7300FF',
+    10 : '#CC00FF',
+    11 : '#FF00EA',
+    12 : '#FF007B'
+}
--- Scripts/ProcessResolverApp.py
+++ Scripts/ProcessResolverApp.py
@@ -4,84 +4,21 @@ from Excell_Writer import Writer
 import Utils
 import Constants
 
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY)
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY)
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY)
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY)
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY)
+def get_processes():
+    return [
+        Process("A", 0, 3),
+        Process("B", 2, 6),
+        Process("C", 4, 4),
+        Process("D", 6, 5),
+        Process("E", 8, 2)
+    ]
 
-writer1 = []
-"--- FIFO ---"
-writer1.append(CPU(Constants.FIFO, [p1, p2, p3, p4, p5, p6, p7]))
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY)
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY)
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY)
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY)
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY)
-"--- SJF ---"
-writer1.append(CPU(Constants.SJF, [p1, p2, p3, p4, p5, p6, p7]))
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY)
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY)
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY)
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY)
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY)
-"--- SRTF ---"
-writer1.append(CPU(Constants.SRTF, [p1, p2, p3, p4, p5, p6, p7]))
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY)
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY)
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY)
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY)
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY)
-"--- RR Q=3 ---"
-writer1.append(CPU(Constants.RR, [p1, p2, p3, p4, p5, p6, p7], 3))
+def get_exercises():
+    return [
+        CPU(Constants.FIFO, get_processes()),
+        CPU(Constants.SJF, get_processes()),
+        CPU(Constants.SRTF, get_processes()),
+        CPU(Constants.RR, get_processes(), 2)
+    ]
 
-"NOW WITH IO" #DOESN'T SEEM TO BE WORKING
-writer2 = []
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY, [[1, 5]])
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY, [[1, 1]])
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY, [[3, 2]])
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY, [[2, 1], [5, 4]])
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY, [[1, 10]])
-
-"--- FIFO ---"
-writer2.append(CPU(Constants.FIFO, [p1, p2, p3, p4, p5, p6, p7]))
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY, [[1, 5]])
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY, [[1, 1]])
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY, [[3, 2]])
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY, [[2, 1], [5, 4]])
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY, [[1, 10]])
-"--- SJF ---"
-writer2.append(CPU(Constants.SJF, [p1, p2, p3, p4, p5, p6, p7]))
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY, [[1, 5]])
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY, [[1, 1]])
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY, [[3, 2]])
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY, [[2, 1], [5, 4]])
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY, [[1, 10]])
-"--- SRTF ---"
-writer2.append(CPU(Constants.SRTF, [p1, p2, p3, p4, p5, p6, p7]))
-p1 = Process("A", 0, 3, Constants.LOW_PRIORITY, [[1, 5]])
-p2 = Process("B", 2, 2, Constants.TOP_PRIORITY, [[1, 1]])
-p3 = Process("C", 4, 4, Constants.MID_PRIORITY, [[3, 2]])
-p4 = Process("D", 1, 1, Constants.TOP_PRIORITY)
-p5 = Process("E", 0, 2, Constants.MID_PRIORITY)
-p6 = Process("F", 2, 10, Constants.TOP_PRIORITY, [[2, 1], [5, 4]])
-p7 = Process("G", 3, 3, Constants.LOW_PRIORITY, [[1, 10]])
-"--- RR Q=4 ---"
-writer2.append(CPU(Constants.RR, [p1, p2, p3, p4, p5, p6, p7], 4))
-
-Writer("Ejercicio 1", writer1, True)
-Writer("Ejercicio 1", writer2, True)
+Writer("Ejercicio_1", get_exercises())
