3a1637a767ebbdc5a47520999f84cee3ab20d5fa
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2020-11-12 18:08:11 +0100

    Need to end GUI functionality

--- Process_Resolver/AddProcessFrame.py
+++ Process_Resolver/AddProcessFrame.py
@@ -7,6 +7,9 @@ class AddProcessFrame(tk.Frame):
         self.init_labels()
         self.init_entries() 
         self.init_pid_selector()
+        self.init_priority_selector()
+        self.init_back_button()
+        self.init_add_button()
 
     def init_labels(self):
         top_label = tk.Label(self, text="Add Process", font=GUIConstants.LARGE_FONT)
@@ -16,7 +19,7 @@ class AddProcessFrame(tk.Frame):
         label_4 = tk.Label(self, text="Process Priority", font=GUIConstants.MEDIUM_FONT)
         label_5 = tk.Label(self, text="Process IO Exits", font=GUIConstants.MEDIUM_FONT)
         label_6 = tk.Label(self, text="Process IO Durations", font=GUIConstants.MEDIUM_FONT)
-        top_label.grid(row=0, column=0)
+        top_label.grid(row=0, column=0, columnspan=2)
         label_1.grid(row=1, column=0)
         label_2.grid(row=2, column=0)
         label_3.grid(row=3, column=0)
@@ -29,6 +32,22 @@ class AddProcessFrame(tk.Frame):
         self.pid.set(GUIConstants.PID_OPTIONS[0])
         pid_selector = tk.OptionMenu(self, self.pid, *GUIConstants.PID_OPTIONS)
         pid_selector.grid(row=1, column=1)
+
+    def init_priority_selector(self):
+        self.priority = tk.StringVar(self)
+        self.priority.set(GUIConstants.PRIORITY_OPTIONS[0])
+        priority_selector = tk.OptionMenu(self, self.priority, *GUIConstants.PRIORITY_OPTIONS)
+        priority_selector.grid(row=4, column=1)
+
+    def init_back_button(self):
+        self.back = tk.Button(self)
+        self.back.configure(text="Back")
+        self.back.grid(row=7, column=0, padx=10, pady=20)
+
+    def init_add_button(self):
+        self.add = tk.Button(self)
+        self.add.configure(text="Add")
+        self.add.grid(row=7, column=1, padx=10, pady=0)
 
     def init_entries(self):
         self.arrival_entry = tk.Entry(self)
--- Process_Resolver/Controller.py
+++ Process_Resolver/Controller.py
@@ -17,6 +17,8 @@ class Controller():
     def init_add_process_frame(self):
         self.add_process_frame = AddProcessFrame(self.get_gui_container(), self)
         self.add_process_frame.grid(row=0, column=0, sticky="nsew")
+        self.add_process_frame.back.configure(command=self.go_to_main_frame)
+        self.add_process_frame.add.configure(command=self.add_process_to_list)
 
     def init_main_frame(self):
         self.main_frame = MainFrame(self.get_gui_container(), self)
@@ -24,21 +26,37 @@ class Controller():
         self.main_frame.del_button.configure(command=self.del_process)
         self.main_frame.ini_button.configure(command=self.ini_process)
         self.main_frame.grid(row=0, column=0, sticky="nsew")
-
-    def show_frame(self, frame_to_show):
-        frame_to_show.tkraise()
         
     def add_process(self):
         self.show_frame(self.add_process_frame)
 
+    def add_process_to_list(self):
+        self.main_frame.process_list.insert(tk.END, " " + self.format_process())
+
+    def format_process(self):
+        process = ""
+        process += self.add_process_frame.pid.get() + " | "
+        process += self.add_process_frame.arrival_entry.get() + " | "
+        process += self.add_process_frame.duration_entry.get() + " | "
+        process += self.add_process_frame.priority.get() + " | "
+        process += self.add_process_frame.io_exits_entry.get() + " | "
+        process += self.add_process_frame.io_duration_entry.get()
+        return process
+
     def del_process(self):
-        print("DEL")
+        self.main_frame.process_list.delete(self.main_frame.process_list.curselection())
 
     def ini_process(self):
         print("INI")
 
+    def go_to_main_frame(self):
+        self.show_frame(self.main_frame)
+
     def get_gui_container(self):
         return self.GUI.container
+
+    def show_frame(self, frame_to_show):
+        frame_to_show.tkraise()
 
     def start_gui(self):
         self.GUI.mainloop()
